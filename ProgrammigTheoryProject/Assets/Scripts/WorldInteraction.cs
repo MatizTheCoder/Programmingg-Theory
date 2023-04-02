7827dc6399ad95fb260ed2d98363644592ad28ab
MatizTheCoder <107365670+MatizTheCoder@users.noreply.github.com>
2023-04-02 13:30:40 +0300

    interaction scripts and nav

---
+++ ProgrammigTheoryProject/Assets/Scripts/WorldInteraction.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !UnityEngine.EventSystems.EventSystem .current.IsPointerOverGameObject());
+        GetInteraction();
+    }
+    void GetInteraction()
+    {
+        Ray interactionRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit interactionInfo;
+        if(Physics.Raycast(interactionRay, out interactionInfo, Mathf.Infinity))
+        {
+            GameObject interactedObject = interactionInfo.collider.gameObject;
+            if(interactedObject.tag == "Interactable Object")
+            {
+                Debug.Log("Interactable interacted");
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
